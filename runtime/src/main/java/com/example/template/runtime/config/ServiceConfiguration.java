e0ed6d0dd9718d0a3f50909427749a74566f4a6f
zo <zoli.konyves@pm.me>
2025-12-30 11:33:19 +0000

    renamed configuration - removing all instances of *UseCase naming

--- runtime/src/main/java/com/example/template/runtime/config/ServiceConfiguration.java
+++ runtime/src/main/java/com/example/template/runtime/config/ServiceConfiguration.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class UseCaseConfiguration {
+public class ServiceConfiguration {
     @Bean
     public GreetingService greetingService(GreetingStore greetingStore) {
         return new GreetingServiceImpl(greetingStore);
